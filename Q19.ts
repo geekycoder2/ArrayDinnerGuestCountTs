21cd24b783b3b7809681fc4986a226884f7dc1f3
Sidra Gondal <150411149+geekycoder2@users.noreply.github.com>
2024-02-17 01:27:08 +0500

    Add files via upload

---
+++ Q19.ts
@@ -0,0 +1,27 @@
+// 19.Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+// Importing Guest list from excercise 15
+
+// let guestArr: string[] = ["Liza", "Alex", "Moon"];
+// let canNotAttend: string = "Jack";
+// let newGuest: string = "Jhon";
+
+// // Replace the guest who cannot attend with a new guest
+// guestArr[guestArr.indexOf(canNotAttend)] = newGuest;
+
+// // Add a new guest to the beginning of the array
+// guestArr.unshift("Rids");
+
+// // Add a middle guest
+// let middleGuest: string = "Petter";
+// let middleIndex = Math.floor(guestArr.length / 2);
+// guestArr.splice(middleIndex, 0, middleGuest);
+
+// // Add another guest to the end of the array
+// guestArr.push("Rock");
+
+// // Print the total number of guests invited to dinner
+// console.log(guestArr.length + " " + "people are invited to dinner");
+
+// // Print the updated guest list
+// console.log(guestArr);
+
